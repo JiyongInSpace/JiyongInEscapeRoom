307fb6efbfd3880e593b92d0b554f4758130c6cd
Jiyong Kim <gd693020@gmail.com>
2024-06-21 14:28:26 +0900

    [INSTALL] vue3

--- frontend/app/api/auth/[...nextauth]/route.ts
+++
@@ -1,7 +0,0 @@
-import { authOptions } from "@/lib/helpers/authOptions"; 
-import NextAuth from "next-auth";
-
-const handler = NextAuth(authOptions) as any;
-
-export { handler as GET, handler as POST };
-
--- frontend/app/root-provider.tsx
+++
@@ -1,8 +0,0 @@
-"use client";
-// 1. import `ChakraProvider` component
-import { ChakraProvider } from "@chakra-ui/react";
-
-export default function RootProviders({ children }: { children: React.ReactNode }) {
-  // 2. Wrap ChakraProvider at the root of your app
-  return <ChakraProvider>{children}</ChakraProvider>;
-}
